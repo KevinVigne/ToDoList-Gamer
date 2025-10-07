2036bef5b527c940c82756c59f9cb1d8f1c2a061
KevinVigne <kev.vigne@gmail.com>
2025-10-07 15:27:57 +0200

    Ajout de chemin vers page d'accueil

---
+++ src/Views/home.view.php
@@ -0,0 +1,10 @@
+<?php
+require_once (__DIR__.'/partials/head.view.php');
+?>
+<h1>Bienvenue</h1>
+<div class="container">
+    
+</div>
+<?php
+// require_once (__DIR__.'/partials/footer.view.php');
+?>
